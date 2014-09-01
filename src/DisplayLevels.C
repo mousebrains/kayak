bcd85f7625e79e35c6984e64c304163d97139300
Pat Welch <pat@mousebrains.com>
2014-08-31 21:44:29 -0700

    mkTable

--- src/DisplayLevels.C
+++ src/DisplayLevels.C
@@ -20,10 +20,10 @@ namespace {
                            (level == Levels::HIGH ? " class='hi'" : "")));
     const bool qLink(!hash.empty() && !type.empty());
     std::string str("<td" + color + ">");
-    if (qLink) str += "<a href='?o=" + hash + "&amp;t=" + type + "'>";
+    if (qLink) str += "<a href='?o=p&amp;h=" + hash + "&amp;t=" + type + "'>";
     str += val;
     if (qLink) str += "</a>";
-    str += "<td>";
+    str += "</td>";
     return str;
   }
 
@@ -51,14 +51,39 @@ namespace {
   }
 
   int mkTable(const Levels& levels) {
+    const std::string title(levels.state().empty() ? "WKCC Water Levels" : levels.state());
+
     HTML html;
+    html << html.header()
+         << "<title>" << title << "</title>\n"
+         << html.myStyle()
+         << html.myScript()
+         << "</head>\n<body>\n"
+         << "<h1>" << title << "</h1>\n"
+         << "<h3>Courtesy of <a href='http://www.wkcc.org'>WKCC</a>, "
+         << "please contact "
+         << "<a href='mailto:pat.kayak@gmail.com'>Pat Welch</a>"
+         << " with any comments about this page.</h3>\n"
+         << "<h4><a href='?o=m'>For other states</a></h4>\n"
+         << "<table>\n"
+         << "<thead>\n";
+
+    std::string hdr("<tr><th>Name</th><th>Date</th>");
+
+    if (levels.qFlow()) hdr += "<th>Flow</br>CFS</th>";
+    if (levels.qGauge()) hdr += "<th>Height</br>Feet</th>";
+    if (levels.qTemperature()) hdr += "<th>Temp</br>F</th>";
+    if (levels.qClass()) hdr += "<th>Class</th>";
+    hdr += "</tr>\n";
+
+    html << hdr << "<tbody>\n";
  
     for (Levels::const_iterator it(levels.begin()), et(levels.end()); it != et; ++it) {
       const bool qf(levels.qFlow() && !isnan(it->flow));
       const bool qg(levels.qGauge() && !isnan(it->gauge));
       const bool qt(levels.qTemperature() && !isnan(it->temperature));
       const std::string& hash(Master::mkHash(it->key));
-  
+ 
       html << "<tr>"
            << "<th><a href='?o=i&h=" << hash << "'>" << it->name;
 
@@ -66,7 +91,8 @@ namespace {
 
       html << "</a></th>";
      
-      formatTime(qf ? it->flowTime : 0, qg ? it->gaugeTime : 0, qt ? it->temperatureTime: 0); 
+      html << formatTime(qf ? it->flowTime : 0, qg ? it->gaugeTime : 0, 
+                         qt ? it->temperatureTime: 0); 
   
       if (qf) {
         html << formatFlow(it->flow, hash, it->level);
@@ -88,16 +114,44 @@ namespace {
 
       html << "\n";
     }
+    html << "</tbody>\n<tfoot>\n" 
+         << hdr;
+        
+    if (levels.qFlow() || levels.qGauge()) { // Low/Okay/High
+      const int n(2 + levels.qFlow() + levels.qGauge() + levels.qTemperature() + levels.qClass());
+      html << "<tr><th class='lo' colspan=" << n << ">Low</th></tr>\n" 
+           << "<tr><th class='ok' colspan=" << n << ">Okay</th></tr>\n" 
+           << "<tr><th class='hi' colspan=" << n << ">High</th></tr>\n";
+    }
+ 
+    html << "</foot>\n"
+         << "</table>\n"
+         << "<h3>Generated " << Convert::toStr(time(0), "%c") << "</h3>\n"
+         << "<dl>\n"
+         << "<dd><span class='lev1'>&uarr;&darr;</span> Changing between 0.5% and 1.5% per hour</dd>\n"
+         << "<dd><span class='lev2'>&uarr;&darr;</span> Changing between 1.5% and 2.5% per hour</dd>\n"
+         << "<dd><span class='lev3'>&uarr;&darr;</span> Changing between 2.5% and 3.5% per hour</dd>\n"
+         << "<dd><span class='lev4'>&uarr;&darr;</span> Changing between 3.5% and 4.5% per hour</dd>\n"
+         << "<dd><span class='lev5'>&uarr;&darr;</span> Changing between 4.5% and 5.5% per hour</dd>\n"
+         << "<dd><span class='lev6'>&uarr;&darr;</span> Changing between 5.5% and 6.5% per hour</dd>\n"
+         << "<dd><span class='lev7'>&uarr;&darr;</span> Changing between 6.5% and 7.5% per hour</dd>\n"
+         << "<dd><span class='lev8'>&uarr;&darr;</span> Changing between 7.5% and 8.5% per hour</dd>\n"
+         << "<dd><span class='lev9'>&uarr;&darr;</span> Changing between 8.5% and 9.5% per hour</dd>\n"
+         << "<dd><span class='lev10'>&uarr;&darr;</span> Changing between 9.5% and 10.5% per hour</dd>\n"
+         << "<dd><span class='lev11'>&uarr;&darr;</span> Changing more than  10.5% per hour</dd>\n"
+         << "</dl>\n"
+         << "</body>\n"
+         << "</html>\n";
+    
     return HTTP(std::cout).htmlPage(86400, html);
   }
 
   int process(const Levels& levels) {
-    if (cgi.qJavaScript()) {
-      levels.json(std::cout) << std::endl;
-    } else {
-      mkTable(levels);
-    }
-    return 0;
+    // if (cgi.qJavaScript()) {
+      // levels.json(std::cout) << std::endl;
+    // } else {
+    return mkTable(levels);
+    // }
   }
 }
 
