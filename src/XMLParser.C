1bb59fa8f17277759d0c36fa2bb74d5199266431
Pat Welch <pat@mousebrains.com>
2014-09-07 15:33:51 -0700

    Rename from JSONParser to XMLParser, what it should have been

--- src/XMLParser.C
+++ src/XMLParser.C
@@ -1,6 +1,6 @@
-#include "JSONParser.H"
+#include "XMLParser.H"
 
-JSONParser::JSONParser(const std::string& str)
+XMLParser::XMLParser(const std::string& str)
   : mContent(str.begin(), str.end())
 {
   mDoc.parse<0>(&mContent[0]); 
