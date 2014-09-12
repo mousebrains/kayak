30c1aab7d0513217dfecb264a5ca037b86f9b0bd
Pat Welch <pat@mousebrains.com>
2014-09-12 07:53:24 -0700

    Handle timezones a bit better

--- src/Convert.C
+++ src/Convert.C
@@ -3,57 +3,198 @@
 #include <sstream>
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <map>
 
 namespace {
- const char *format[] = {
-    "%Y%b%d %H:%M",
-    "%Y-%m-%d %H:%M:%S",
-    "%Y%m%d%H%M%S",
-    "%Y%m%d%H%M",
-    "%Y%m%d%H",
-    "%Y%m%d",
-    "%m/%d/%Y %H%M%S",
-    "%m/%d/%Y %H:%M:%S",
-    "%m/%d/%Y %H%M",
-    "%m/%d/%Y %H:%M",
-    "%m/%d.%H:%M",
-    "%m/%d%H:%M",
-    "%d%b%Y%H%M%S",
-    "%d%b%Y%H%M",
-    "%I%M %p PST %a %b %d %Y", // 130 PM PST SAT APR 01 2006
-    "%I%M %p PDT %a %b %d %Y", // 130 PM PDT SUN APR 02 2006
-    "%Y %a %b%d %H:%M:%S", // 2006 Wed Mar21 12:00
-    "%Y %a %b%d %H:%M", // 2006 Wed Mar21 12:00
-    "%d %b %Y %H:%M:%S", // 03 APR 2006
-    "%d %b %Y %H:%M", // 03 APR 2006
-    "%a %b %d, %Y %H:%M", // MONDAY APRIL 3, 2006 00:00
-    "%Y-%m-%dT%H:%M:%S-00:00", // 2006-03-26T04:00:00-00:00
-    "%Y-%m-%dT%H:%M:%SZ", // 2010-09-24T04:30:00Z
-    "%Y-%m-%d", // 2010-09-24
-    "%m-%d-%Y", // 9-24-2010
-    "%m/%d/%Y", // 9/24/2010
-    0};
+  time_t tzOffset(const std::string& tz) {
+    typedef std::map<std::string, int> tMap;
+    static const tMap tzMap = {
+        {"edt", -4}, {"-04:00", -4},
+        {"est", -5}, {"-05:00", -5},
+        {"cdt", -5}, {"-05:00", -5},
+        {"cst", -6}, {"-06:00", -6},
+        {"mdt", -6}, {"-06:00", -6},
+        {"mst", -7}, {"-07:00", -7},
+        {"pdt", -7}, {"-07:00", -7},
+        {"pst", -8}, {"-08:00", -8}
+      };
+    tMap::const_iterator it(tzMap.find(Convert::tolower(tz)));
+    if (it != tzMap.end()) return it->second * 3600;
+    std::cerr << "Timezone '" << tz << "' not found" << std::endl;
+    exit(1);
+  }
+
+  bool getNumber(const std::string& str,
+                 size_t offset,
+                 int& value,
+                 int nDigits,
+                 const int llim,
+                 const int ulim)
+    {
+      value = 0;
+      for (size_t e(str.size()); offset < e && nDigits; ++offset, --nDigits) {
+        const char c(str[offset]);
+        if ((c < '0') || (c > '9')) return false;
+        value = (value * 10) + (c - '0');
+      }
+      return (nDigits == 0) && (value >= llim) && (value <= ulim);
+    }
+
+  size_t getZone(std::string str, std::string& tz) {
+    size_t n(0);
+    std::string::size_type i(str.find_first_not_of(" \t\n\r"));
+    if (i != str.npos) {
+      n = i; // How much whitespace I am taking off the front
+      str = str.substr(i);
+    }
+    i = str.find_first_of("\t\n\r");
+    tz = str.substr(0,i);
+    return n + tz.size();
+  }
+
+  size_t getMonth(std::string str, int &mon) {
+    static std::vector<std::vector<std::string> >  a = {
+      {0, {"january", "jan"}},
+      {1, {"febuary", "feb"}},
+      {2, {"march", "mar"}},
+      {3, {"april", "apr"}},
+      {4, {"may"}},
+      {5, {"june", "jun"}},
+      {6, {"july", "jul"}},
+      {7, {"august", "aug"}},
+      {8, {"september", "sep"}},
+      {9, {"october", "oct"}},
+      {10, {"november", "nov"}},
+      {11, {"december", "dec"}},
+      };
+
+    size_t n(0);
+    { // Strip off leading whitespace
+      std::string::size_type i(str.find_first_not_of(" \t\n\r"));
+      if (i != str.npos) { // Strip off leading whitespace
+        n = i;
+        str = str.substr(i);
+      }
+    }
+    str = Convert::tolower(str);
+    for (size_t i(0), e(a.size()); i < e; ++i) { // Walk through months
+      const std::vector<std::string>& b(a[i]);
+      for (size_t j(0), je(a[i].size()); j < je; ++j) { // Walk through representations
+        if (str.find(b[j]) == 0) {
+          mon = i;
+          return n + b[j].size();
+        }
+      }
+    }
+    std::cerr << "Unrecognized month '" << str << "'" << std::endl;
+    return 0;
+  }
+
+  bool mkTime(const std::string& str, const std::string& fmt, time_t& t) {
+    std::string tz;
+    struct tm tm({0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+    size_t j(0), je(str.size());;
+     
+    for (size_t i(0), e(fmt.size()), state(0); (i < e) && (j < je); ++i) {
+      if (state == 0) { // Looking for a % in fmt
+        if (fmt[i] == '%') {
+          state = 1;
+          continue;
+        }
+        if (isspace(fmt[i])) { // A whitespace, so gobble up what there is in str
+          while (isspace(str[j]) && (j < je)) {
+            ++j;
+          }
+        } else if (fmt[i] != str[j++]) { // Check for character match, then push j forwards
+          return false; // Characters did not match
+        }
+        continue;
+      }
+      state = 0;
+      switch (fmt[i]) {
+        case '%': 
+          if (str[j++] != '%') return false; // Check for % and then push j forwards
+          break;
+        case 'Y': 
+          if (!getNumber(str, j, tm.tm_year, 4, 1900, 2100)) return false; // Not a valid year
+          tm.tm_year -= 1900;
+          j += 4;
+          break;
+        case 'm': 
+          if (!getNumber(str, j, tm.tm_mon, 2, 1, 12)) return false; // Not a valid month
+          --tm.tm_mon; // set to [0,11] instead of [1,12]
+          j += 2;
+          break;
+        case 'd': 
+          if (!getNumber(str, j, tm.tm_mday, 2, 1, 31)) return false; // Not a valid month day
+          j += 2;
+          break;
+        case 'H': 
+          if (!getNumber(str, j, tm.tm_hour, 2, 0, 24)) return false; // Not a valid hour
+          j += 2;
+          break;
+        case 'M': 
+          if (!getNumber(str, j, tm.tm_min, 2, 0, 59)) return false; // Not a valid minute
+          j += 2;
+          break;
+        case 'S': 
+          if (!getNumber(str, j, tm.tm_sec, 2, 0, 59)) return false; // Not a valid second
+          j += 2;
+          break;
+        case 'B':   // full month name
+        case 'b': { // abbreviated month name
+            size_t n(getMonth(str.substr(j), tm.tm_mon));
+            if (!n) return false;
+            j += n;
+          }
+          break;
+        case 'Z': 
+        case 'z': {
+            size_t n(getZone(str.substr(j), tz));
+            if (tz.empty()) return false;
+            j += n;
+          }
+          break;
+        default: 
+          std::cerr << "Unsupported time format '" << fmt[i] << "' in " << fmt << std::endl;
+          exit(1);
+      } // switch
+    } // for
+
+    if (j == je) { // Parsed cleanly, now check if tz specified
+      if (tz.empty()) { // no tz, so use mktime
+        t = mktime(&tm);
+        return true;
+      }
+      t = timegm(&tm) - tzOffset(tz); // Convert assuming UTC and add back in timezone offset
+      return true;
+    }
+  
+    if (j > je) { // past
+      std::cout << "Ate too much";
+    } else {
+      std::cout << "Leftover(" << str.substr(j) << ")";
+    }
+    std::cout << " str(" << str << ") fmt(" << fmt << ")" << std::endl;
+    return false;
+  } // mkTime
 } // anonymous
 
 time_t
 Convert::toTime(const std::string& str)
 {
-  const char *txt(str.c_str());
-  const time_t now(time(0));
-  struct tm reftm;
-  localtime_r(&now, &reftm);
-  reftm.tm_sec = 0; // Set reference time to midnight
-  reftm.tm_min = 0;
-  reftm.tm_hour = 0;
+  static const std::vector<std::string> formats = { // Longest first
+    "%Y-%m-%dT%H:%M:%S%z", // will also swallow %Z
+    "%Y-%m-%dT%H:%M:%S",
+    "%Y-%m-%d %H:%M:%S",
+    };
 
-  for (size_t i(0); format[i]; ++i) {
-    struct tm tm = reftm;
-    const char *ptr(strptime(txt, format[i], &tm));
-    if (ptr && (*ptr == 0)) { // Found a match
-      tm.tm_sec = 0; // Zero out the seconds
-      return mktime(&tm);
-    }
+  for (size_t i(0), e(formats.size()); i < e; ++i) {
+    time_t t;
+    if (mkTime(str, formats[i], t)) return t;
   }
+  std::cerr << "Unable to parse '" << str << "'" << std::endl;
   return 0;
 }
 
