fb4200f50ec254f685d0642f1ead16f1be69ad6d
Pat Welch <pat@mousebrains.com>
2014-09-12 07:53:40 -0700

    verbose options

--- src/Curl.C
+++ src/Curl.C
@@ -1,11 +1,24 @@
 #include "Curl.H"
 #include "MyException.H"
+#include "Convert.H"
+#include "ReadFile.H"
 #include <iostream>
 
-Curl::Curl(const std::string& url)
+Curl::Curl(const std::string& url,
+           const bool qVerbose)
   : mOkay(false)
   , mURL(url)
+  , mResponseCode(0)
 {
+  mErrorMsg[0] = '\0'; // Zero terminate the error message stream
+
+  if (Convert::tolower(url.substr(0,9)).find("file:///") == 0) { // local file
+    mText = ReadFile(url.substr(7));
+    mOkay = true;
+    mResponseCode = 200;
+    return;
+  }
+
   CURL *curl(curl_easy_init()); // Initialize a curl handle
 
   if (!curl) { // Failed
@@ -17,6 +30,8 @@ Curl::Curl(const std::string& url)
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // Skip any certificate verification
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); // Skip hostname verification
   curl_easy_setopt(curl, CURLOPT_ENCODING, ""); // Enable compression
+  curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, mErrorMsg); // Where to store the error message
+  curl_easy_setopt(curl, CURLOPT_VERBOSE, qVerbose ? 1L : 0L); // Enable/disable verbose messages to stderr
 
   // Now setup the callback for receiving the data
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
@@ -32,6 +47,45 @@ Curl::Curl(const std::string& url)
     errchk(curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ptr), 
            "curl_easy_getinfo(CURLINFO_CONTENT_TYPE");
     mContentType = ptr;
+  }
+
+  if (qVerbose) { // Dump diagnostic information, beyond CURLOPT_VERBOSE
+    { // Get timestamp of file retrieved
+      long a;
+      errchk(curl_easy_getinfo(curl, CURLINFO_FILETIME, &a), "curl_easy_getinfo"); 
+      std::cout << "Curl FileTime " << Convert::toStr((time_t) a, "%c") << std::endl;
+    }
+    { // Times to fetch the document
+      double t;
+      errchk(curl_easy_getinfo(curl, CURLINFO_NAMELOOKUP_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl name lookup time " << t << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_CONNECT_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl connect time " << t << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_APPCONNECT_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl appconnect time " << t << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_PRETRANSFER_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl pre-transfer time " << t << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_STARTTRANSFER_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl start transfer time " << t << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_REDIRECT_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl redirect time " << t << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &t), "curl_easy_getinfo");
+      std::cout << "Curl total time " << t << std::endl;
+    }
+    { // size of the body retrieved
+      double sz, sp;
+      errchk(curl_easy_getinfo(curl, CURLINFO_SIZE_UPLOAD, &sz), "curl_easy_getinfo");
+      errchk(curl_easy_getinfo(curl, CURLINFO_SPEED_UPLOAD, &sp), "curl_easy_getinfo");
+      std::cout << "Curl size uploaded " << sz 
+                << " at " << sp << " bytes/sec" << std::endl;
+      errchk(curl_easy_getinfo(curl, CURLINFO_SIZE_DOWNLOAD, &sz), "curl_easy_getinfo");
+      errchk(curl_easy_getinfo(curl, CURLINFO_SPEED_DOWNLOAD, &sp), "curl_easy_getinfo");
+      std::cout << "Curl size downloaded " << sz 
+                << " at " << sp << " bytes/sec " 
+                << " expanded size " << mText.size()
+                << " compression ratio " << ((double) sz / mText.size() * 100) << "%"
+                << std::endl;
+    }
   }
 
   curl_easy_cleanup(curl); // Clean everything up
