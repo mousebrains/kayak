ffc2c98083bdf647c2055b9f6d8fc24576ab909a
Pat Welch <pat@mousebrains.com>
2014-09-13 16:58:18 -0700

    Add support for NOAA0 and USGS1

---
+++ src/ParserNOAA0.C
@@ -0,0 +1,113 @@
+// Parse a NOAA XML site data file, a gauge specific rss file
+
+#include "ParserNOAA0.H"
+#include "XMLParser.H"
+#include "Gauges.H"
+#include "GaugeTranslate.H"
+#include "Convert.H"
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+namespace {
+  GaugeTranslate translate;
+
+  std::string capitalize(const std::string& str) {
+    if (str.empty()) return str; 
+    std::string a(Convert::tolower(str));
+    if ((a[0] >= 'a') && (a[0] <= 'z')) a[0] = a[0] - 'a' + 'A';
+    return a;
+  }
+} // anonymous
+
+ParserNOAA0::ParserNOAA0(const std::string& url,
+                         const std::string& str,
+                         const bool qVerbose)
+  : mURL(url)
+{
+  const XMLParser xml(str);
+  for (XMLParser::Node root(xml.first()); root; ++root) { // HydroMetData
+    if (strcmp(root.name(), "HydroMetData")) continue;
+    for (XMLParser::Node site(root.firstChild()); site; ++site) { // SiteData 
+      if (strcmp(site.name(), "SiteData")) continue;
+      collection(site);
+    }
+  }
+}
+
+void
+ParserNOAA0::collection(const XMLParser::Node& site)
+{
+  Gauges::Info info;
+  info.idCBTT = site.findAttr("id");
+
+  std::string description;
+
+  for (XMLParser::Node node(site.firstChild()); node; ++node) { // Walk through site information
+    const std::string name(node.name());
+    const std::string val(node.value());
+    if (name == "description") { description = val;
+    } else if (name == "county") { info.county = capitalize(val);
+    } else if (name == "state") { info.state = capitalize(val);
+    } else if (name == "elevation") { info.elevation = Convert::strTo<double>(val);
+    } else if (name == "latitude") { info.latitude = Convert::strTo<double>(val);
+    } else if (name == "longitude") { info.longitude = Convert::strTo<double>(val);
+    } else if (name == "bankfullStage") { info.bankfullStage = Convert::strTo<double>(val);
+    } else if (name == "floodStage") { info.floodStage = Convert::strTo<double>(val);
+    } else if (name == "observedData") {
+      observedData(node, info.idCBTT);
+    } else if (name == "forecastData") {
+      ; // Do nothing
+    } else {
+      std::cerr << "Unrecognized name(" << name << ") val(" << val << ")" << std::endl;
+    }
+  }
+
+  info.gaugeKey = mGauges.name2key(info.idCBTT);
+  if (info.gaugeKey <= 0) { // Didn't find
+    info.gaugeKey = mGauges.idCBTT2gaugeKey(info.idCBTT);
+  }
+
+  if (info.gaugeKey > 0) { // Found a gauge key
+    const GaugeTranslate::Info a(translate(description));
+    info.description = a.description;
+    info.location = a.location;
+    mGauges.putInfo(info);
+  }
+}
+
+void
+ParserNOAA0::observedData(const XMLParser::Node& root,
+                          const std::string& name)
+{
+  for (XMLParser::Node node(root.firstChild()); node; ++node) {
+    if (!strcmp(node.name(), "observedValue")) observedValue(node, name);
+  }
+}
+
+void
+ParserNOAA0::observedValue(const XMLParser::Node& root,
+                          const std::string& name)
+{
+  double flow(NAN), gauge(NAN);
+  time_t t(0);
+
+  for (XMLParser::Node node(root.firstChild()); node; ++node) {
+    const std::string name(node.name());
+    const std::string val(node.value());
+    if (name == "dataDateTime") {
+      t = Convert::toTime(val);
+    } else if (name == "stage") {
+      gauge = Convert::strTo<double>(val);
+    } else if (name == "discharge") {
+      flow = Convert::strTo<double>(val);
+    } else {
+      std::cerr << "Unrecognized node name(" << name << ") value(" << val << ")" << std::endl;
+    } 
+  }
+
+  if (t > 0) {
+    if (~isnan(flow)) mData.add(name, t, flow, Data::FLOW, mURL);
+    if (~isnan(gauge)) mData.add(name, t, gauge, Data::GAUGE, mURL);
+  }
+}
---
+++ src/ParserUSGS1.C
@@ -0,0 +1,93 @@
+// Parse a USGS XML site information file 
+
+#include "ParserUSGS1.H"
+#include "XMLParser.H"
+#include "Gauges.H"
+#include "GaugeTranslate.H"
+#include "StateCounty.H"
+#include "Convert.H"
+#include <iostream>
+#include <sstream>
+
+namespace {
+  GaugeTranslate translate;
+  StateCounty stateCounty;
+} // anonymous
+
+ParserUSGS1::ParserUSGS1(const std::string& url,
+                         const std::string& str,
+                         const bool qVerbose)
+{
+  const XMLParser xml(str);
+  for (XMLParser::Node root(xml.first()); root; ++root) { // usgs_nwis level
+    for (XMLParser::Node site(root.firstChild()); site; ++site) { // site 
+      collection(site);
+    }
+  }
+}
+
+void
+ParserUSGS1::collection(const XMLParser::Node& site)
+{
+  static const std::set<std::string> toIgnore = {
+      "agency_cd", 
+      "alt_datum_cd", 
+      "land_net_ds", 
+      "site_tp_cd", 
+      "coord_acy_cd", 
+      "dec_lat_long_datum_cd",
+      "district_cd",
+      "alt_acy_va",
+      "basin_cd",
+      "topo_cd",
+      "construction_dt",
+      "inventory_dt",
+      "contrib_drain_area_va",
+      };
+
+  Gauges::Info info;
+  std::string state, county, description;
+
+  for (XMLParser::Node node(site.firstChild()); node; ++node) { // Walk through site information
+    const std::string name(node.name());
+    if (toIgnore.find(name) != toIgnore.end()) continue;
+    const std::string val(node.value());
+    if (name == "site_no") { 
+      info.name = val;
+      info.idUSGS = val;
+    } else if (name == "station_nm") { description = val;
+    } else if (name == "dec_lat_va") { info.latitude = Convert::strTo<double>(val);
+    } else if (name == "dec_long_va") { info.longitude = Convert::strTo<double>(val);
+    } else if (name == "state_cd") { state = val;
+    } else if (name == "county_cd") { county = val;
+    } else if (name == "alt_va") { info.elevation = Convert::strTo<double>(val);
+    } else if (name == "huc_cd") { info.idUnit = val;
+    } else if (name == "drain_area_va") { info.drainageArea = Convert::strTo<double>(val);
+    } else {
+       std::cerr << "Unregonized USGS site field(" << name << ") value(" << val << ")" << std::endl;
+    }
+  }
+
+  info.gaugeKey = mGauges.name2key(info.name, false);
+  if (info.gaugeKey <= 0) { // Look up by idUSGS
+    info.gaugeKey = mGauges.idUSGS2gaugeKey(info.name);
+  }
+
+  if (info.gaugeKey > 0) { // Found a gauge to put information into
+    if (!description.empty()) { // Translate string
+      const GaugeTranslate::Info a(translate(description));
+      info.description = a.description;
+      info.location = a.location;
+    }
+
+    if (!state.empty()) {
+      const int skey(Convert::strTo<int>(state));
+      info.state = stateCounty.state(skey);
+      if (!county.empty()) {
+        const int ckey(Convert::strTo<int>(county));
+        info.county = stateCounty.county(skey, ckey);
+      }
+    }
+    mGauges.putInfo(info);
+  }
+}
