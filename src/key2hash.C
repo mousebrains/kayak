440958c3798d1117585ec2dfb6a116991329ad7d
Pat Welch <pat@mousebrains.com>
2014-09-14 15:13:50 -0700

    Add hash to key conversion

--- src/key2hash.C
+++ src/key2hash.C
@@ -1,20 +1,33 @@
 #include "Master.H"
 #include "Convert.H"
 #include <iostream>
+#include <getopt.h>
 
 int
 main (int argc,
       char **argv)
 {
-  for (int i(1); i < argc; ++i) {
-    const size_t val(atoi(argv[i]));
-    const std::string str(Master::mkHash(val));
-    
-    std::cout << "arg[" << i << "]=" << val << " hash '" 
-              << str
-              << "' dehash "
-              << Master::deHash(str)
-              << std::endl;
+  const char *options("k");
+  bool qKey2Hash(true);
+
+  for (int c; (c = getopt(argc, argv, options)) != EOF;) {
+    qKey2Hash = !qKey2Hash;
+  }
+ 
+  for (int i(optind); i < argc; ++i) {
+    if (qKey2Hash) {
+      const size_t val(atoi(argv[i]));
+      const std::string str(Master::mkHash(val));
+      std::cout << "argv[" << i << "]=" << val << " hash '" 
+                << str
+                << "' dehash "
+                << Master::deHash(str)
+                << std::endl;
+    } else { // Hash 2 key
+      const size_t key(Master::deHash(argv[i]));
+      const std::string hash(Master::mkHash(key));
+      std::cout << "argv[" << i << "]='" << argv[i] << "' key " << key << " hash(" << hash << ")" << std::endl;
+    }
   }
 
   return 0;
