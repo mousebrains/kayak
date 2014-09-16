581dc309701c75b1f9d6aa17932876243475acdc
Pat Welch <pat@mousebrains.com>
2014-09-16 13:30:28 -0700

    Pull tolower from Convert into String

--- src/String.C
+++ src/String.C
@@ -14,3 +14,24 @@ String::trim(const std::string& str)
 
   return str.substr(i0, i1 - i0 + 1); // leading and trailing whitespaces stripped
 }
+
+std::string
+String::tolower(const std::string& str)
+{
+  std::string result(str);
+
+  for (std::string::size_type i(0), e(result.size()); i < e; ++i) {
+    result[i] = std::tolower(result[i]);
+  }
+
+  return result;
+}
+
+std::string
+String::capitalize(const std::string& str)
+{
+  if (str.empty()) return str;
+  std::string result(tolower(str));
+  result[0] = std::toupper(result[0]);
+  return result;
+}
