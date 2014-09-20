66f51b60187278f6a8a2d8d0b4a3157eeedf432a
Pat Welch <pat@mousebrains.com>
2014-09-20 09:12:14 -0700

    Add dirname and tail

--- src/File.C
+++ src/File.C
@@ -17,3 +17,37 @@ File::mtime() const
 {
   return mOkay ? mStat.st_mtime : 0;
 }
+
+std::string
+File::dirname(const std::string& fn)
+{
+  const std::string::size_type i(fn.rfind('/'));
+
+  if (i == fn.npos)
+    return "./";
+
+  if (i != (fn.size() - 1))  // Not a trailing /
+    return fn.substr(0, i + 1);
+
+  if (fn.size() == 1) // Only a /
+    return fn;
+
+  return dirname(fn.substr(0, fn.size() - 1)); // strip off trailing / and try again
+}
+
+std::string
+File::tail(const std::string& fn)
+{
+  const std::string::size_type i(fn.rfind('/'));
+
+  if (i == fn.npos)
+    return fn;
+
+  if (i != (fn.size() - 1))  // Not a trailing /
+    return fn.substr(i+1);
+
+  if (fn.size() == 1) // Only a /
+    return fn;
+
+  return tail(fn.substr(0, fn.size() - 1)); // strip off trailing / and try again
+}
