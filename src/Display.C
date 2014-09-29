b4bb49c6a76cc318ab2e4feb3bfdbf21571b2342
Pat Welch <pat@mousebrains.com>
2014-09-29 10:49:03 -0700

    maybes pulled in and DisplayGauges

--- src/Display.C
+++ src/Display.C
@@ -3,6 +3,8 @@
 #include "CGI.H"
 #include "HTML.H"
 #include "Convert.H"
+#include "Calc.H"
+#include "Master.H"
 #include <cmath>
 #include <sstream>
 
@@ -106,4 +108,252 @@ Display::dateForm(const DataGet& obs,
   oss << "</div>\n";
 
   return oss.str();
+}
+
+bool
+Display::maybeLatLon(std::ostream& os,
+                     const double lat,
+                     const double lon,
+                     const std::string& prefix,
+                     const std::string& suffix)
+{
+  if (isnan(lat) || isnan(lon) || (fabs(lat) > 90) || (fabs(lon) > 180)) return false;
+
+  os << "<li>" 
+     << prefix
+     << "<a href='" << HTML::mapURL(lat, lon) << "'>"
+     << Convert::toLatLon(lat) << ", " << Convert::toLatLon(lon)
+     << "</a>"
+     << " "
+     << "<a href='" << HTML::weatherURL(lat, lon) << "'> Weather Forecast</a>"
+     << suffix 
+     << "</li>\n";
+
+  return true;
+}
+
+bool
+Display::maybeLatLon(std::ostream& os,
+                     const double lat0,
+                     const double lon0,
+                     const double lat1,
+                     const double lon1,
+                     const std::string& prefix,
+                     const std::string& suffix)
+{
+  const bool q0(isnan(lat0) || isnan(lon0) || (fabs(lat0) > 90) || (fabs(lon0) > 180));
+  const bool q1(isnan(lat1) || isnan(lon1) || (fabs(lat1) > 90) || (fabs(lon1) > 180));
+
+  if (q0 && q1) return false;
+  if (q0) return maybeLatLon(os, lat1, lon1, prefix, suffix);
+  if (q1) return maybeLatLon(os, lat0, lon0, prefix, suffix);
+
+  os << "<li>"
+     << prefix
+     << "<a href='" << HTML::mapURL(lat0, lon0, lat1, lon1) << "'>"
+     << Convert::toLatLon(lat0) << ", " << Convert::toLatLon(lon0)
+     << " to "
+     << Convert::toLatLon(lat1) << ", " << Convert::toLatLon(lon1)
+     << "</a>"
+     << " "
+     << "<a href='" << HTML::weatherURL((lat0 + lat1) / 2, (lon0 + lon1) / 2) 
+     << "'> Weather Forecast</a>"
+     << suffix 
+     << "</li>\n";
+    
+  return true;
+}
+
+bool
+Display::maybe(std::ostream& os,
+               const double min,
+               const double max,
+               const std::string& prefix,
+               const std::string& suffix)
+{
+  const bool qMin(!isnan(min) && min != 0);
+  const bool qMax(!isnan(max) && max != 0);
+
+  if (!qMin && !qMax) return false;
+
+  os << "<li>" << prefix;
+
+  if (qMin) {
+    if (qMax) {
+      os << min << " to " << max;
+    } else {
+      os << min;
+    }
+  } else {
+    os << max;
+  }
+
+  os << suffix << "</li>\n";
+
+  return true;
+}
+
+bool
+Display::maybeMinMax(std::ostream& os,
+                     const double min,
+                     const double max,
+                     const std::string& prefix,
+                     const std::string& suffix)
+{
+  const bool qMin(!isnan(min));
+  const bool qMax(!isnan(max));
+
+  if (!qMin && !qMax) return false;
+
+  os << "<li>" << prefix;
+
+  if (qMin) {
+    if (qMax) {
+      os << "[" << min << ", " << max << "]";
+    } else {
+      os << ">= " << min;
+    }
+  } else {
+    os << "<= " << max;
+  }
+
+  os << suffix << "</li>\n";
+
+  return true;
+}
+
+bool 
+Display::maybeCalc(std::ostream& os, 
+                   const std::string& str,
+                   Master& master,
+                   const std::string& prefix,
+                   const std::string& suffix) 
+{
+  if (str.empty()) return false;
+  Calc calc(0, str, Data::FLOW);
+
+  os << "<li>" 
+     << prefix;
+ 
+  for (Calc::size_type i(0), e(calc.size()); i < e; ++i) {
+    if (calc[i].qRef()) {
+      const int gaugeKey(calc[i].key());
+      os << "<a href='?o=g&amp;h=" << Master::mkHash(gaugeKey) << "'>"
+         << calc[i].str()
+         << "</a>";
+    } else {
+      os << calc[i].str();
+    }
+  } // for
+  os << suffix 
+     << "</li>\n";
+  return true;
+} // maybeCalc
+
+template<>
+bool
+Display::maybe(std::ostream& os,
+               const int& val,
+               const std::string& prefix,
+               const std::string& suffix)
+{
+  if (val == 0) return false;
+
+  os << "<li>"
+     << prefix
+     << val
+     << suffix
+     << "</li>\n";
+
+  return true;
+}
+
+template<>
+bool
+Display::maybe(std::ostream& os,
+               const size_t& val,
+               const std::string& prefix,
+               const std::string& suffix)
+{
+  if (val == 0) return false;
+
+  os << "<li>"
+     << prefix
+     << val
+     << suffix
+     << "</li>\n";
+
+  return true;
+}
+
+template<>
+bool
+Display::maybe(std::ostream& os,
+               const double& val,
+               const std::string& prefix,
+               const std::string& suffix)
+{
+  if (isnan(val)) return false;
+
+  os << "<li>"
+     << prefix
+     << val
+     << suffix
+     << "</li>\n";
+
+  return true;
+}
+
+template<>
+bool
+Display::maybe(std::ostream& os,
+               const std::string& str,
+               const std::string& prefix,
+               const std::string& suffix)
+{
+  if (str.empty()) return false;
+
+  os << "<li>"
+     << prefix
+     << str
+     << suffix
+     << "</li>\n";
+
+  return true;
+}
+
+template<>
+bool
+Display::maybe(std::ostream& os,
+               const time_t& t,
+               const std::string& prefix,
+               const std::string& suffix)
+{
+  if (t <= 0) return false;
+
+  os << "<li>"
+     << prefix
+     << Convert::toStr(t, "%Y-%m-%d %H:%M")
+     << suffix
+     << "</li>\n";
+
+  return true;
+}
+
+bool
+Display::maybeHRef(std::ostream& os,
+                   const std::string& str,
+                   const std::string& url,
+                   const std::string& prefix,
+                   const std::string& suffix)
+{
+  if (str.empty() || url.empty()) return maybe(os, str, prefix, suffix);
+
+  os << "<li>"
+     << prefix
+     << "<a href='" << url << "'>" << str << "</a>"
+     << suffix
+     << "</li>\n";
+
+  return true;
 }
