21390c19180a61bfcdaf7a96ec20579ed2688ef4
Pat Welch <pat@mousebrains.com>
2014-09-29 10:45:08 -0700

    Display gauge information

---
+++ src/DisplayGauges.C
@@ -0,0 +1,117 @@
+#include "Display.H"
+#include "Gauges.H"
+#include "Master.H"
+#include "Levels.H"
+#include "Tokens.H"
+#include "CGI.H"
+#include "HTML.H"
+#include "HTTP.H"
+#include "Convert.H"
+#include <iostream>
+#include <cmath>
+
+namespace {
+  int mkTable(const Gauges::tInfo& info, 
+              const Master::tKey2KeyNameLocation& keyMap, 
+              Master& master,
+              const Levels& levels) {
+    typedef std::map<int, size_t> tMap2Level;
+    tMap2Level map2Level;
+
+    for (Levels::size_type i(0), e(levels.size()); i < e; ++i) {
+      map2Level.insert(std::make_pair(levels[i].gaugeKey, i));
+    }
+
+    HTML html;
+    html << html.header()
+         << html.myStyle()
+         << "<title>Gauges</title>\n"
+         << "</head>\n<body>\n";
+
+    bool qHR(false);
+    for (Gauges::tInfo::const_iterator it(info.begin()), et(info.end()); it != et; ++it) {
+      const Gauges::Info& a(*it);
+      if (qHR) html << "<hr>\n";
+      qHR = true;
+      html << "<div>\n"
+           << "<h2>" << a.description << " " << a.location << "</h2>\n";
+
+      { // Display level information, if known
+        tMap2Level::const_iterator jt(map2Level.find(a.gaugeKey));
+        if (jt != map2Level.end()) {
+          levels[jt->second].table(html);
+        }
+      }
+
+      html << "<ul>\n";
+      Display::maybe(html, a.name, "Name: ");
+      { // Get river segments that use this gauge
+        typedef Master::tKey2KeyNameLocation::const_iterator const_iterator;
+        std::pair<const_iterator, const_iterator> iters(keyMap.equal_range(a.gaugeKey));
+        for (const_iterator jt(iters.first), jet(iters.second); jt != jet; ++jt) {
+          const Master::KeyNameLocation& b(jt->second);
+          html << "<li>Run: <a href='?o=i&amp;h=" << Master::mkHash(b.key)
+               << "'>" << b.name
+               << " " 
+               << (b.location.empty() ? a.location : b.location) 
+               << "</a></li>\n";
+        }
+      }
+      Display::maybeLatLon(html, a.latitude, a.longitude, "Lat/Lon: ");
+      Display::maybe(html, a.state, "State: ");
+      Display::maybe(html, a.county, "County: ");
+      Display::maybe(html, a.elevation, "Elevation: ", " Feet");
+      Display::maybe(html, a.drainageArea, "Drainage Area: ", " Square Miles");
+      Display::maybe(html, a.bankfullStage, "Bank Full Stage: ", " Feet");
+      Display::maybe(html, a.date, "Last updated: ");
+      Display::maybeCalc(html, a.calcFlow, master, "Flow Calculation: ");
+      Display::maybeCalc(html, a.calcGauge, master, "Gauge Calculation: ");
+      Display::maybe(html, a.gaugeKey, "Gauge Key: ");
+      Display::maybe(html, a.idUSGS, "USGS ID: ");
+      Display::maybe(html, a.idCBTT, "CBTT ID: ");
+      Display::maybe(html, a.idUSBR, "USBR ID: ");
+      Display::maybe(html, a.idUnit, "Hydrologic Unit: ");
+      Display::maybeMinMax(html, a.minFlow, a.maxFlow, "Flow Limits: ", " CFS");
+      Display::maybeMinMax(html, a.minGauge, a.maxGauge, "Gauge Limits: ", " Feet");
+      Display::maybeMinMax(html, a.minTemperature, a.maxTemperature, "Temperature Limits: ", " F");
+      html << "</ul>\n</div>\n";
+    }
+
+    html << "</body>\n</html>\n";
+    return HTTP(std::cout).htmlPage(86400, html); 
+  }
+} // Anonymous
+
+int
+Display::gauges()
+{
+  const CGI cgi;
+  MyDB::Stmt::tInts keys;
+
+  if (cgi.isSet("h")) {
+    const Tokens tokens(cgi.get("h", std::string()), " ,\n\t");
+    MyDB::Stmt::tInts keys;
+    for (Tokens::const_iterator it(tokens.begin()), et(tokens.end()); it != et; ++it) {
+      keys.push_back(Master::deHash(*it));
+    }
+  // } else { // Look for like hash
+    // const Tokens tokens(cgi.get("l", std::string()), " ,\n\t");
+    // MyDB::Stmt::tInts keys;
+    // for (Tokens::const_iterator it(tokens.begin()), et(tokens.end()); it != et; ++it) {
+      // keys.push_back(Master::deHash(*it));
+    // }
+    // info = gauges.getLike(keys);
+  }
+
+  Gauges gauges;
+  const Gauges::tInfo info(gauges.getInfo(keys));
+
+  for (Gauges::tInfo::const_iterator it(info.begin()), et(info.end()); it != et; ++it) {
+    keys.push_back(it->gaugeKey);
+  }
+
+  Master master;
+  const Master::tKey2KeyNameLocation keyMap(master.gaugeKey2KeyNameLocation(keys));
+
+  return mkTable(info, keyMap, master, Levels(keys));
+}
